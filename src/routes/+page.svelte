1fb0658309a9399dca252be99de49d7e1aa92ade
idambaliharyono <idambaliharyono01@gmail.com>
2026-05-10 22:19:08 +0800

    continuing landing page, compeleting heros layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -46,78 +46,89 @@
 </script>
 
 <!-- hero section -->
-<section class="relative h-screen w-screen overflow-hidden">
+<section class="relative h-[70vh] w-screen overflow-hidden bg-amber-200">
 	<div
-		class="flex h-full w-full transition-transform duration-500"
+		class="flex h-full w-full transition-transform duration-1000"
 		style={`transform: translateX(-${current * 100}%);`}
 	>
 		{#each slides as item}
 			<div class="flex h-full min-w-full flex-col">
 				<!-- slide content -->
-				<div class="relative flex h-[40%] w-full shrink-0">
+				<div class="relative flex h-[75%] w-full shrink-0">
 					<img
 						src={placeholder1}
 						alt="placeholder1"
 						class="absolute inset-0 h-full w-full object-cover object-center"
 					/>
 					<div class="absolute inset-0 z-10 bg-black/20"></div>
-					<!-- 				<div class="relative z-10 flex h-full flex-col justify-center px-12 text-white"> -->
 					<div
-						class="relative inset-4 z-10 flex h-full w-[50%] flex-col justify-center text-xl text-white"
+						class="relative inset-4 z-10 flex h-full w-[50%] flex-col justify-center text-4xl text-white"
 					>
 						{item.subtitle.toUpperCase()}
 					</div>
 				</div>
-				<div class="bg-gray-100 p-4 text-center">{item.paragraph}</div>
+				<div class="flex min-h-[25%] flex-col justify-center bg-gray-500 px-6 text-justify">
+					<p class="">
+						{item.paragraph}
+					</p>
+				</div>
 			</div>
 		{/each}
 		<!-- slide button -->
 	</div>
-	<button onclick={nextSlide} class="absolute top-[35%] right-0 z-50 size-10 bg-gray-500">
-		&gt;
+	<button
+		onclick={nextSlide}
+		class="
+			absolute
+			top-[65%]
+			right-4
+			z-50
+			-translate-y-1/2
+			rounded-full
+			bg-black/40
+			p-4
+			text-white
+			backdrop-blur-sm
+			transition-all
+			duration-300
+			hover:scale-110
+			hover:bg-black/60
+			active:scale-95
+		"
+	>
+		>>
 	</button>
 </section>
-
-<p>
-	Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed, debitis? Eum ullam reiciendis fugit
-	necessitatibus, soluta similique blanditiis, in maiores voluptate maxime excepturi harum dolorum.
-	Veritatis in mollitia eligendi deserunt.
-</p>
-<!-- <section class="relative h-screen overflow-hidden"> -->
-<!-- 	<div class="flex h-full"> -->
-<!-- 		{#each body as item} -->
-<!-- 			<!-- slide --> -->
-<!-- 			<div class="relative min-w-full shrink-0"> -->
-<!-- 				<!-- background image --> -->
-<!-- 				<img -->
-<!-- 					src={item.image} -->
-<!-- 					alt={item.subtitle} -->
-<!-- 					class="absolute inset-0 h-full w-full object-cover" -->
-<!-- 				/> -->
-<!---->
-<!-- 				<!-- dark overlay --> -->
-<!-- 				<div class="absolute inset-0 bg-black/40"></div> -->
-<!---->
-<!-- 				<!-- content --> -->
-<!-- 				<div class="relative z-10 flex h-full flex-col justify-center px-12 text-white"> -->
-<!-- 					<!-- subtitle row --> -->
-<!-- 					<h2 class="text-5xl font-bold"> -->
-<!-- 						{item.subtitle} -->
-<!-- 					</h2> -->
-<!---->
-<!-- 					<!-- paragraph row --> -->
-<!-- 				</div> -->
-<!-- 				<p class="mt-6 max-w-xl text-lg"> -->
-<!-- 					{item.paragraph} -->
-<!-- 				</p> -->
-<!-- 			</div> -->
-<!-- 		{/each} -->
-<!-- 	</div> -->
-<!---->
-<!-- 	<!-- navigation --> -->
-<!-- 	<button -->
-<!-- class="absolute top-1/2 right-4 z-20 -translate-y-1/2 rounded-full bg-white/20 p-4 text-white backdrop-blur" -->
-<!--  	>  -->
-<!-- &gt; -->
-<!--  	</button>  -->
-<!-- </section> -->
+<section class="flex h-screen w-screen flex-col justify-center px-2">
+	<h1 class="text-center text-3xl">Sacred Herbs for Body, Mind, and Soul</h1>
+	<img src={placeholder2} class="my-6 bg-amber-200 object-cover object-center" alt="" />
+	<p class="text-center">
+		Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed, debitis? Eum ullam reiciendis
+		fugit necessitatibus, soluta similique blanditiis, in maiores voluptate maxime excepturi harum
+		dolorum. Veritatis in mollitia eligendi deserunt.
+	</p>
+</section>
+<section class="">
+	<div class="flex flex-col justify-center gap-2 bg-gray-200 px-2 text-center">
+		<h2>100% Rempah Organik</h2>
+		<p>Ditanam oleh petani lokal Indonesia</p>
+		<div class="flex gap-1">
+			<div class="rounded-xl bg-gray-400 p-2">
+				<img src={placeholder2} alt="placeholder" />
+				<p>Coconut oil</p>
+			</div>
+			<div class="rounded-xl bg-gray-400 p-2">
+				<img src={placeholder2} alt="placeholder" />
+				<p>Coconut oil</p>
+			</div>
+			<div class="rounded-xl bg-gray-400 p-2">
+				<img src={placeholder2} alt="placeholder" />
+				<p>Coconut oil</p>
+			</div>
+			<div class="rounded-xl bg-gray-400 p-2">
+				<img src={placeholder2} alt="placeholder" />
+				<p>Coconut oil</p>
+			</div>
+		</div>
+	</div>
+</section>
