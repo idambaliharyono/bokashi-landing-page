02d587c3cdb58124fba4096816577e70b4a7e2ac
idambaliharyono <idambaliharyono01@gmail.com>
2026-06-04 05:45:10 +0800

    creating some components

---
+++ src/lib/components/CenterText.svelte
@@ -0,0 +1,30 @@
+<!-- Hero.svelte -->
+<script>
+	let {
+		heading = 'this is heading',
+		body = [],
+		headingSizeMobile = '2xl',
+		headingSizeDesktop = '2xl'
+	} = $props();
+
+	const hadingClass = `text-${headingSizeMobile} lg:text-${headingSizeDesktop}`;
+</script>
+
+<div class="mt-4 flex w-full flex-col justify-center gap-4 px-2 text-center text-primary lg:px-10">
+	<div
+		class="gap-1/3 mx-4 flex flex-col justify-center font-heading
+    text-2xl font-black tracking-tight
+    lg:flex-row lg:gap-2"
+	>
+		<h2 class={hadingClass}>{heading}</h2>
+	</div>
+	<div class="flex flex-col gap-4">
+		<div class="w-full font-body">
+			{#each body as paragraph}
+				<p>
+					{paragraph}
+				</p>
+			{/each}
+		</div>
+	</div>
+</div>
--- src/routes/about-us/+page.svelte
+++ src/routes/about-us/+page.svelte
@@ -2,6 +2,38 @@
 	import Hero from '$lib/components/Hero.svelte';
 
 	import hero from '$lib/assets/slides3.jpg';
+	import CenterText from '$lib/components/CenterText.svelte';
+
+	const heroNumbers = [
+		{
+			number: '28',
+			body: 'Years of Experience'
+		},
+		{
+			number: '5000',
+			body: 'EM Training Alumnee'
+		},
+		{
+			number: '10 Mio',
+			body: 'Sold Products'
+		}
+	];
 </script>
 
 <Hero src={hero} alt="images">OUR STORY OF PURPOSE</Hero>
+
+<CenterText
+	heading="FROM VILLAGE WISDOM TO GLOBAL WELLNESS"
+	headingSize="rounded-2xl"
+	body={[
+		'Terinspirasi dari ramuan herbal Dadong Bandung di Desa Bengkel, Pak Oles memadukan kearifan lokal dengan ilmu pengetahuan dan teknologi modern dari Jepang, untuk melahirkan Minyak Bokashi.'
+	]}
+/>
+<div class="mt-4 grid grid-cols-3 gap-2 p-1 lg:gap-12 lg:px-12">
+	{#each heroNumbers as item}
+		<div class="rounded-2xl bg-secondary px-4 py-4 text-center text-primary lg:px-12">
+			<h4 class="text-xl font-bold">{item.number}+</h4>
+			<p>{item.body}</p>
+		</div>
+	{/each}
+</div>
